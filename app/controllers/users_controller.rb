9d4049aec8150a11df78e81a102acf6116ca1619
leo-holanda <leo.uldholanda@gmail.com>
2019-10-03 14:56:33 -0300

    add edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
     def new
         @user = User.new
     end
@@ -13,7 +14,21 @@ class UsersController < ApplicationController
         end
     end
 
-    private 
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+    
+        if @user.update(user_params)
+          redirect_to edit_user_path(@user)
+        else
+          render :edit
+        end
+    end
+
+    private
 
     def user_params
         params.require(:user).permit(:username,:email,:password)
